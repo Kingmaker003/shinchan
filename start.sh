fe9694e8ad8627c682c4babb43e96f6a92f767d2
Kingmaker003 <107481248+Kingmaker003@users.noreply.github.com>
2022-10-05 07:55:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Ajax
 fi
-cd /Ajax
+cd /shinchan
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
